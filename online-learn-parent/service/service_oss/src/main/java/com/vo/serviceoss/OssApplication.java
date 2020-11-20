818ba2917d0bc230dc4ebb10d63dcd247f72d30d
yinuo100823 <534003512@qq.com>
2020-11-20 18:39:52 +0800

    添加注册中心

--- online-learn-parent/service/service_oss/src/main/java/com/vo/serviceoss/OssApplication.java
+++ online-learn-parent/service/service_oss/src/main/java/com/vo/serviceoss/OssApplication.java
@@ -3,12 +3,14 @@ package com.vo.serviceoss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //exclude = DataSourceAutoConfiguration.class不加载数据库信息
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.vo"})
 @EnableSwagger2
+@EnableDiscoveryClient
 public class OssApplication {
     public static void main(String[] args) {
         SpringApplication.run(OssApplication.class, args);
