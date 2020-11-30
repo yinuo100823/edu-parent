b810b5ad0f3ef61b032d28c53df1e48988186c5f
yinuo100823 <534003512@qq.com>
2020-11-30 15:02:09 +0800

    首页数据增加redis缓存

---
+++ online-learn-parent/service/service_cms/src/main/java/com/vo/servicecms/mapper/CrmBannerMapper.java
@@ -0,0 +1,16 @@
+package com.vo.servicecms.mapper;
+
+import com.vo.servicecms.entity.CrmBanner;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 首页banner表 Mapper 接口
+ * </p>
+ *
+ * @author Loong
+ * @since 2020-11-23
+ */
+public interface CrmBannerMapper extends BaseMapper<CrmBanner> {
+
+}
